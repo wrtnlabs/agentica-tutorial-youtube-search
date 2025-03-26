46edea09ee2a7be9ced17a6d4675bf49a1f69f53
Asher <asher@wrtn.io>
2025-03-26 18:21:05 +0900

    add: youtube search tutorial

---
+++ READEME.md
@@ -0,0 +1,8 @@
+# agentica-tutorial-crawler
+
+Assign your OpenAI API key into the .env.sample file.
+
+After modifying the .env.sample, please rename it .env.
+
+And when you run the file, the results will be displayed in the terminal console.
+
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {Agentica} from "@agentica/core";
+import dotenv from "dotenv";
+import {OpenAI} from "openai";
+import typia from "typia";
+import {YoutubeSearchService} from "@wrtnlabs/connector-youtube-search";
+
+dotenv.config();
+
+const agent: Agentica<"chatgpt"> = new Agentica({
+    model: "chatgpt",
+    vendor: {
+        api: new OpenAI({apiKey: process.env.OPENAI_API_KEY!}),
+        model: "gpt-4o-mini",
+    },
+    controllers: [
+        {
+            name: "Google Scholar Connector",
+            protocol: "class",
+            application: typia.llm.application<YoutubeSearchService, "chatgpt">(),
+            execute: new YoutubeSearchService({
+                serpApiKey: process.env.SERP_API_KEY!,
+                googleApiKey: process.env.GOOGLE_API_KEY!,
+                searchApiKey: process.env.SEARCH_API_KEY!,
+            }),
+        },
+    ],
+});
+
+const main = async () => {
+    console.log(await agent.conversate("What can you do?"));
+};
+
+main();
